b3e22fb783399ef14befeb04a6fb4749bf79ea44
chanyu <trafalgarlaw0816@gmail.com>
2024-04-26 15:37:51 +0800

    add image_for_download

--- app.py
+++ app.py
@@ -153,6 +153,24 @@ def create_tables():
 def index():
     return render_template('index.html')
 
+@app.route('/save_image_for_download', methods=['POST'])
+def save_image_for_download():
+    data = request.json
+    image_data = data['image_data']
+    image_name = data["image_name"]
+    image_name = re.sub(r'\s+', '_', image_name)
+    image_folder_path = os.path.join("Annotations", f"Server_images")
+    if not os.path.exists(image_folder_path):
+        os.mkdir(image_folder_path)
+        
+    output_path = os.path.join(image_folder_path, f'{image_name}.jpg')
+    image_data = image_data.split(",")[1]  # Remove the prefix of Base64 encoding if present
+    image_bytes = base64.b64decode(image_data)
+    
+    image = pilImage.open(io.BytesIO(image_bytes))
+    image = image.convert('RGB')
+    image.save(output_path)
+
 @app.route('/save_image', methods=['POST'])
 def save_image():
     data = request.json
